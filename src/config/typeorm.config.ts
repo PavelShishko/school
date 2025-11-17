d2315e802a3d4831f7e3c27c5289428cda12421d
Pavel Shishko <pashasosiska@gmail.com>
2025-11-17 11:49:47 +0300

    edit: edit typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,14 +4,16 @@ import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export async function getTypeOrmConfig (config : ConfigService) : Promise<TypeOrmModuleOptions> {
     return {
         type: 'postgres',
-        host: config.getOrThrow('POSTGRES_HOST'),
-        port: config.getOrThrow('POSTGRES_PORT'),
-        username: config.getOrThrow('POSTGRES_USER'),
-        password: config.getOrThrow('POSTGRES_PASSWORD'),
-        database: config.getOrThrow('POSTGRES_DATABASE'),
-        entities: ['dist/**/*.entity.js'], // для runtime
-        migrations: ['dist/migrations/*.js'],
-        synchronize: false,
+        url: process.env.DATABASE_URL,
+        // host: config.getOrThrow('POSTGRES_HOST'),
+        // port: config.getOrThrow('POSTGRES_PORT'),
+        // username: config.getOrThrow('POSTGRES_USER'),
+        // password: config.getOrThrow('POSTGRES_PASSWORD'),
+        // database: config.getOrThrow('POSTGRES_DATABASE'),
+        // entities: ['dist/**/*.entity.js'], // для runtime
+        // migrations: ['dist/migrations/*.js'],
+        autoLoadEntities: true,
+        synchronize: true,
         logging: true,
     }
 }
